d5685ba25b30e7ef9995d58e85a7e4266e61ad39
Paweł Cichowski <pawel.cichowski@3mdeb.com>
2022-08-02 10:02:17 +0200

    import new custom tool

--- README.md
+++ README.md
@@ -1,67 +1,43 @@
+# Headless Chrome PDF Maker
 
-# Usage: 
-```
-chrome-headless-render-pdf [OPTIONS] --url=URL --pdf=OUTPUT-FILE [--url=URL2 --pdf=OUTPUT-FILE2] ...
-  Options:
-    --help                   this screen
-    --url                    url to load, for local files use: file:///path/to/file
-    --pdf                    output for generated file can be relative to current directory
-    --chrome-binary          set chrome location (use this options when autodetection fail)
-    --chrome-option          set chrome option, can be used multiple times, e.g. --chrome-option=--no-sandbox
-    --remote-host            set chrome host (for remote process)
-    --remote-port            set chrome port (for remote process)
-    --no-margins             disable default 1cm margins
-    --include-background     include elements background
-    --landscape              generate pdf in landscape orientation
-    --window-size            specify window size, width(,x*)height (e.g. --window-size 1600,1200 or --window-size 1600x1200)
-    --paper-width            specify page width in inches (defaults to 8.5 inches)
-    --paper-height           specify page height in inches (defaults to 11 inches)
-    --page-ranges            specify pages to render default all pages,  e.g. 1-5, 8, 11-13
-    --scale                  specify scale of the webpage rendering (defaults to 1)
-    --display-header-footer  display text headers and footers
-    --header-template        HTML template for the header. Inject variables using the classes "date", "title", "url", "pageNumber" or "totalPages"
-    --footerTemplate         HTML template for the footer. Inject variables using the classes "date", "title", "url", "pageNumber" or "totalPages"
-    
-  Example:
-    Render single pdf file
-      chrome-headless-render-pdf --url http://google.com --pdf test.pdf
-    Render pdf from local file
-      chrome-headless-render-pdf --url file:///tmp/example.html --pdf test.pdf
-    Render multiple pdf files
-      chrome-headless-render-pdf --url http://google.com --pdf test.pdf --url file:///tmp/example.html --pdf test2.pdf
-    Render pdf with custom footer and no header (styles are mandatory)
-      chrome-headless-render-pdf --url file:///tmp/example.html --pdf test.pdf --display-header-footer --header-template ' ' --footer-template '<style type="text/css">.footer{font-size:8px;width:100%;text-align:center;color:#000;padding-left:0.65cm;}</style><div class="footer"><span class="pageNumber"></span> / <span class="totalPages"></span></div>'
-```
+## Usage:
 
-# This tool can be also used programmatically:
-```
-const RenderPDF = require('chrome-headless-render-pdf');
-RenderPDF.generateSinglePdf('http://google.com', 'outputPdf.pdf');
-```
+> Note: this server requires the HTML to be provided by a server, it doesn't 
+work on bare .html files. The easiest way to do that is to host a local server 
+by running `python3 -m http.server 8000`.
+
+> Then, provide the script with a URL to the file, e.g. `http://127.0.0.1:8000/my-file.html`.
 
 ```
-const RenderPDF = require('chrome-headless-render-pdf');
-RenderPDF.generateMultiplePdf([
-    {'http://google.com', 'outputPdf.pdf'},
-    {'http://example.com', 'outputPdf2.pdf'}
-]);
-```
+node chrome-headless-pdf-maker --url url/to/html --pdf path/for/output/pdf
+Options:
+  -u, --url      URL to html file                            [string] [required]
+  -p, --pdf      path to output pdf                          [string] [required]
+  -f, --format   All the valid paper format types when printing a PDF, a full
+                 list of them at
+                 https://pptr.dev/api/puppeteer.lowercasepaperformat/
+                                                        [string] [default: "a4"]
+      --version  Show version number                                   [boolean]
+      --help     Show help                                             [boolean]
+
+Flags:
+(Can also be executed with --no-[flag] prefix to set the flag to false)
+
+      --display-header-footer  Whether to show the header and footer
+                                                      [boolean] [default: false]
+      --omit-background        Hides default white background and allows
+                               generating pdfs with transparency
+                                                      [boolean] [default: false]
+      --landscape              Whether to print in landscape orientation
+                                                      [boolean] [default: false]
+      --prefer-css-page-size   Give any CSS @page size declared in the page
+                               priority over what is declared in the width or
+                               height or format option [boolean] [default: true]
+      --print-background       Set to true to print background graphics
+                                                       [boolean] [default: true]
 
 ```
-const RenderPDF = require('chrome-headless-render-pdf');
-RenderPDF.generatePdfBuffer('http://google.com')
-    .then((pdfBuffer) => {
-      console.log(pdfBuffer);
-    });
-```
 
-# you can also use it from typescript or es6
-```
-import RenderPDF from 'chrome-headless-render-pdf';
-RenderPDF.generateSinglePdf('http://google.com', 'outputPdf.pdf');
-```
+## Requirements
 
-# Motivation
-google-chrome currently have option to render pdf files when used with headless option. 
-But this option contains hardcoded adding header and footer to page rendering it unusable for pdf generation.
-This module allows to generate it without those elements.
+- Node.js >= 14.10
--- index.js
+++ index.js
@@ -1,408 +1,101 @@
-const CDP = require('chrome-remote-interface');
-const fs = require('fs');
-const cp = require('child_process');
-const net = require('net');
-const commandExists = require('command-exists');
+#!/usr/bin/env node
 
-class StreamReader {
-    constructor(stream) {
-        this.data = '';
-        stream.on('data', (chunk) => {
-            this.data += chunk.toString();
-        });
-    }
+const updateNotifier = require('update-notifier');
+let pkg;
+try {
+    pkg = require('./package.json');
+} catch (e) {
+    pkg = require('./package.json');
 }
+
+updateNotifier({pkg}).notify();
+
+const puppeteer = require('puppeteer');
+
+
+// Handle program arguments with yargs
+const argv = require('yargs')(process.argv.slice(2))
+
+    // Parameters with input arguments
+    .option('url', {
+        alias: 'u',
+        type: 'string',
+        description: 'URL to html file'
+    })
+    .option('pdf', {
+        alias: 'p',
+        type: 'string',
+        description: 'path to output pdf'
+    })
+    .option('format', {
+        alias: 'f',
+        type: 'string',
+        default: 'a4',
+        description: 'All the valid paper format types when printing a PDF, a full list of them at https://pptr.dev/api/puppeteer.lowercasepaperformat/'
+    })
+
+    // Flags
+    .boolean('display-header-footer')
+    .describe('display-header-footer', 'Whether to show the header and footer')
+    .default('display-header-footer', false)
+
+    .boolean('omit-background')
+    .describe('omit-background', 'Hides default white background and allows generating pdfs with transparency')
+    .default('omit-background', false)
+    
+    .boolean('landscape')
+    .describe('landscape', 'Whether to print in landscape orientation')
+    .default('landscape', false)
+    
+    .boolean('prefer-css-page-size')
+    .describe('prefer-css-page-size', 'Give any CSS @page size declared in the page priority over what is declared in the width or height or format option')
+    .default('prefer-css-page-size', true)
+    
+    .boolean('print-background')
+    .describe('print-background', 'Set to true to print background graphics')
+    .default('print-background', true)
+    
+    .group(['url', 'pdf', 'format'], 'Options:')
+    .group(['display-header-footer', 'omit-background', 'landscape', 'prefer-css-page-size', 'print-background'], 'Flags:\n(Can also be executed with --no-[flag] prefix to set the flag to false)\n')
+
+    .demandOption('url', 'Please enter the URL to an HTML file')
+    .demandOption('pdf', 'Please specify the output pdf name or path')
+    .help().argv;
+
 
 class RenderPDF {
-    constructor(options) {
-        this.setOptions(options || {});
-        this.chrome = null;
 
-        if (this.options.remoteHost) {
-          this.host = this.options.remoteHost;
-          this.port = this.options.remotePort;
-        } else {
-          this.host = 'localhost';
-        }
-    }
-
-    selectFreePort() {
-        return new Promise((resolve) => {
-            let port = Math.floor(Math.random() * 30000) + 30000;
-            const server = net.createServer({allowHalfOpen: true});
-            server.on('listening', () => {
-                server.close(() => {
-                    resolve(port);
-                });
-            });
-            server.on('error', () => {
-                port = Math.floor(Math.random() * 30000) + 30000;
-                server.listen(port);
-            });
-            server.listen(port);
-        })
-    }
-
-    setOptions(options) {
-        this.options = {
-            printLogs: def('printLogs', false),
-            printErrors: def('printErrors', true),
-            chromeBinary: def('chromeBinary', null),
-            chromeOptions: def('chromeOptions', []),
-            remoteHost: def('remoteHost', null),
-            remotePort: def('remotePort', 9222),
-            noMargins: def('noMargins', false),
-            landscape: def('landscape', undefined),
-            paperWidth: def('paperWidth', undefined),
-            paperHeight: def('paperHeight', undefined),
-            includeBackground: def('includeBackground', undefined),
-            pageRanges: def('pageRanges', undefined),
-            scale: def('scale', undefined),
-            displayHeaderFooter: def('displayHeaderFooter', false),
-            headerTemplate: def('headerTemplate', undefined),
-            footerTemplate: def('footerTemplate', undefined),
-            preferCSSPageSize: def('preferCSSPageSize', undefined),
+    static async renderPDF() {
+        const launchParams = {
+            headless: true,
+            slowMo: 0 // TODO remove debug stuff
         };
 
-        this.commandLineOptions = {
-            windowSize: def('windowSize', undefined),
-        };
-
-        function def(key, defaultValue) {
-            return options[key] === undefined ? defaultValue : options[key];
-        }
-    }
-
-    static async generateSinglePdf(url, filename, options) {
-        const renderer = new RenderPDF(options);
-        await renderer.connectToChrome();
-        try {
-            const buff = await renderer.renderPdf(url, renderer.generatePdfOptions());
-            fs.writeFileSync(filename, buff);
-            renderer.log(`Saved ${filename}`);
-        } catch (e) {
-            renderer.error('error:', e);
-        }
-        renderer.killChrome();
-    }
-
-    static async generatePdfBuffer(url, options) {
-        const renderer = new RenderPDF(options);
-        await renderer.connectToChrome();
-        try {
-            return await renderer.renderPdf(url, renderer.generatePdfOptions());
-        } catch (e) {
-            renderer.error('error:', e);
-        } finally {
-            renderer.killChrome();
-        }
-    }
-
-    static async generateMultiplePdf(pairs, options) {
-        const renderer = new RenderPDF(options);
-        await renderer.connectToChrome();
-        for (const job of pairs) {
-            try {
-                const buff = await renderer.renderPdf(job.url, renderer.generatePdfOptions());
-                fs.writeFileSync(job.pdf, buff);
-                renderer.log(`Saved ${job.pdf}`);
-            } catch (e) {
-                renderer.error('error:', e);
-            }
-        }
-        renderer.killChrome();
-    }
-
-    async renderPdf(url, options) {
-        const client = await CDP({host: this.host, port: this.port});
-        this.log(`Opening ${url}`);
-        const {Page, Emulation, LayerTree} = client;
-        await Page.enable();
-        await LayerTree.enable();
-
-        const loaded = this.cbToPromise(Page.loadEventFired);
-        const jsDone = this.cbToPromise(Emulation.virtualTimeBudgetExpired);
-
-        await Page.navigate({url});
-        await Emulation.setVirtualTimePolicy({policy: 'pauseIfNetworkFetchesPending', budget: 5000});
-
-        await this.profileScope('Wait for load', async () => {
-            await loaded;
+        const browser = await puppeteer.launch(launchParams);
+        const page = await browser.newPage();
+        await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
+        await page.goto(argv.url, {
+        waitUntil: 'networkidle2',
         });
-
-        await this.profileScope('Wait for js execution', async () => {
-            await jsDone;
-        });
-
-        await this.profileScope('Wait for animations', async () => {
-            await new Promise((resolve) => {
-                setTimeout(resolve, 5000); // max waiting time
-                let timeout = setTimeout(resolve, 100);
-                LayerTree.layerPainted(() => {
-                    clearTimeout(timeout);
-                    timeout = setTimeout(resolve, 100);
-                });
-            });
-        });
-
-        const pdf = await Page.printToPDF(options);
-        const buff = Buffer.from(pdf.data, 'base64');
-        client.close();
-        return buff;
-    }
-
-    generatePdfOptions() {
-        const options = {};
-        if (this.options.landscape !== undefined) {
-            options.landscape = !!this.options.landscape;
-        }
-
-        if (this.options.noMargins) {
-            options.marginTop = 0;
-            options.marginBottom = 0;
-            options.marginLeft = 0;
-            options.marginRight = 0;
-        }
-
-        if (this.options.includeBackground !== undefined) {
-            options.printBackground = !!this.options.includeBackground;
-        }
-
-        if(this.options.paperWidth !== undefined) {
-            options.paperWidth = parseFloat(this.options.paperWidth);
-        }
-
-        if(this.options.paperHeight !== undefined) {
-            options.paperHeight = parseFloat(this.options.paperHeight);
-        }
-
-        if(this.options.pageRanges !== undefined) {
-            options.pageRanges = this.options.pageRanges;
-        }
-      
-        if (this.options.displayHeaderFooter !== undefined) {
-            options.displayHeaderFooter = !!this.options.displayHeaderFooter;
-        }
         
-        if (this.options.headerTemplate !== undefined) {
-            options.headerTemplate = this.options.headerTemplate;
-        }
-        
-        if (this.options.footerTemplate !== undefined) {
-            options.footerTemplate = this.options.footerTemplate;
-        }
+        await page.emulateMediaType("print");
 
-        if(this.options.scale !== undefined) {
-            let scale = this.options.scale;
-            if(scale < 0.1) {
-                console.warn(`scale cannot be lower than 0.1, using 0.1`);
-                scale = 0.1;
-            }
-            if(scale > 2) {
-                console.warn(`scale cannot be higher than 2, using 2`);
-                scale = 2;
-            }
-            options.scale = scale;
-        }
+        await page.evaluateHandle('document.fonts.ready'); // wait for all fonts to load
 
-        if(this.options.preferCSSPageSize !== undefined) {
-            options.preferCSSPageSize = true;
-        }
-
-        return options;
-    }
-
-    error(...msg) {
-        if (this.options.printErrors) {
-            console.error(...msg);
-        }
-    }
-
-    log(...msg) {
-        if (this.options.printLogs) {
-            console.log(...msg);
-        }
-    }
-
-    async cbToPromise(cb) {
-        return new Promise((resolve) => {
-            cb((resp) => {
-                resolve(resp);
-            })
+        await page.pdf({
+            path: argv.pdf,
+            format: argv.format,
+            displayHeaderFooter: argv.displayHeaderFooter,
+            landscape: argv.landscape,
+            preferCSSPageSize: argv.preferCssPageSize,
+            printBackground: argv.printBackground,
+            omitBackground: argv.omitBackground
         });
-    }
 
-    getPerfTime(prev) {
-        const time = process.hrtime(prev);
-        return time[0] * 1e3 + time[1] / 1e6;
-    }
-
-    async profileScope(msg, cb) {
-        const start = process.hrtime();
-        await cb();
-        this.log(msg, `took ${Math.round(this.getPerfTime(start))}ms`);
-    }
-
-    browserLog(type, msg) {
-        const lines = msg.split('\n');
-        for (const line of lines) {
-            this.log(`(chrome) (${type}) ${line}`);
-        }
-    }
-
-    async spawnChrome() {
-        if(!this.port) {
-            this.port = await this.selectFreePort();
-        }
-        const chromeExec = this.options.chromeBinary || await this.detectChrome();
-        this.log('Using', chromeExec);
-        const commandLineOptions = [
-             '--headless',
-             `--remote-debugging-port=${this.port}`,
-             '--disable-gpu',
-             ...this.options.chromeOptions
-            ];
-
-        if (this.commandLineOptions.windowSize !== undefined ) {
-          commandLineOptions.push(`--window-size=${this.commandLineOptions.windowSize[0]},${this.commandLineOptions.windowSize[1]}`);
-
-        }
-        this.chrome = cp.spawn(
-            chromeExec,
-            commandLineOptions
-        );
-        const stdout = new StreamReader(this.chrome.stdout);
-        const stderr = new StreamReader(this.chrome.stderr);
-        this.chrome.on('close', (code) => {
-            this.log(`Chrome stopped (${code})`);
-            this.browserLog('out', stdout.data);
-            this.browserLog('err', stderr.data);
-        });
-    }
-
-    async connectToChrome() {
-      if (!this.options.remoteHost) {
-        await this.spawnChrome();
-      }
-
-      await this.waitForDebugPort();
-    }
-
-    async isCommandExists(cmd) {
-        return new Promise((resolve, reject) => {
-            commandExists(cmd, (err, exists) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(exists);
-                }
-            })
-        });
-    }
-
-    async detectChrome() {
-        if (await this.isCommandExists('google-chrome-unstable')) {
-            return 'google-chrome-unstable';
-        }
-        if (await this.isCommandExists('google-chrome-beta')) {
-            return 'google-chrome-beta';
-        }
-        if (await this.isCommandExists('google-chrome-stable')) {
-            return 'google-chrome-stable';
-        }
-        if (await this.isCommandExists('google-chrome')) {
-            return 'google-chrome';
-        }
-        if (await this.isCommandExists('chromium')) {
-            return 'chromium';
-        }
-        if (await this.isCommandExists('chromium-browser')) {
-            return 'chromium-browser';
-        }
-        // windows
-        if (await this.isCommandExists('chrome')) {
-            return 'chrome';
-        }
-        if (await this.isCommandExists('C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe')) {
-            return 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
-        }
-        if (await this.isCommandExists('C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe')) {
-            return 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
-        }
-        // macos
-        if (await this.isCommandExists('/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome')) {
-            return '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome';
-        }
-        if (await this.isCommandExists('/Applications/Google\ Chrome\ Dev.app/Contents/MacOS/Google\ Chrome')) {
-            return '/Applications/Google\ Chrome\ Dev.app/Contents/MacOS/Google\ Chrome';
-        }
-        if (await this.isCommandExists('/Applications/Google\ Chrome\ Beta.app/Contents/MacOS/Google\ Chrome')) {
-            return '/Applications/Google\ Chrome\ Beta.app/Contents/MacOS/Google\ Chrome';
-        }
-        if (await this.isCommandExists('/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome')) {
-            return '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome';
-        }
-        throw Error('Couldn\'t detect chrome version installed! use --chrome-binary to pass custom location');
-    }
-
-    killChrome() {
-        if (!this.options.remoteHost) {
-            this.chrome.kill(cp.SIGKILL);
-        }
-    }
-
-    async waitForDebugPort() {
-        this.log('Waiting for chrome to became available');
-        while (true) {
-            try {
-                await this.isPortOpen(this.host, this.port);
-                this.log('Chrome port open!');
-                await this.checkChromeVersion();
-                return;
-            } catch (e) {
-                await this.wait(10);
-            }
-        }
-    }
-
-    async checkChromeVersion() {
-        const client = await CDP({host: this.host, port: this.port});
-        try {
-            const {Browser} = client;
-            const version = await Browser.getVersion();
-            if (version.product.search('/64.') !== -1) {
-                this.error('     ===== WARNING =====');
-                this.error('  Detected Chrome in version 64.x');
-                this.error('  This version is known to contain bug in remote api that prevents this tool to work');
-                this.error('  This issue is resolved in version 65');
-                this.error('  More info: https://github.com/Szpadel/chrome-headless-render-pdf/issues/22');
-            }
-            this.log(`Connected to ${version.product}, protocol ${version.protocolVersion}`);
-        } catch (e) {
-            this.error(`Wasn't able to check chrome version, skipping compatibility check.`);
-        }
-    }
-
-    async isPortOpen(host, port) {
-        return new Promise(function (resolve, reject) {
-            const connection = new net.Socket();
-            connection.connect({host, port});
-            connection.on('connect', () => {
-                connection.end();
-                resolve();
-            });
-            connection.on('error', () => {
-                reject();
-            })
-        });
-    }
-
-    async wait(ms) {
-        return new Promise((resolve) => {
-            setTimeout(resolve, ms);
-        });
+        await browser.close();
     }
 }
 
-module.exports = RenderPDF;
-module.exports.default = RenderPDF;
+(async () => {
+    await RenderPDF.renderPDF();
+})();
